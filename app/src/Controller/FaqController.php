ef3b75daf9fe04d944c7dd18cbca387d026fdcb7
Atef OUERGHI <atef.ouerghi@spa-rac.org>
2020-07-20 14:13:49 +0100

    20/07/2020

---
+++ app/src/Controller/FaqController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FaqController extends AbstractController
+{
+    /**
+     * @Route("/faq", name="faq")
+     */
+    public function index()
+    {
+        return $this->render('faq/index.html.twig');
+    }
+}
